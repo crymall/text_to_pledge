45b06766a03ef55e8fb8c1d407b8c9c70433b3b5
Reed Gaines <reed.gaines@gmail.com>
2018-10-21 21:17:32 -0400

    total?

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
 import axios from "axios";
 import wordmark from "./assets/wordmark.png";
+import { Line } from "rc-progress";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      total: 0
+    };
   }
 
   componentDidMount() {
-    // axios.get("/").then(res => {
-    //   console.log(res);
-    // });
+    axios.get("/total").then(res => {
+      this.setState({
+        total: res.total
+      });
+    });
   }
 
   render() {
+    const { total } = this.state;
     return (
       <div className="App">
         <h1>PURSUIT BASH</h1>
+        <Line
+          className="the-bar"
+          percent={(total / 1000).toString()}
+          strokeWidth="4"
+          strokeColor="#1E1E1E"
+          strokeLinecap="butt"
+          trailColor="#E3E3E3"
+          trailWidth="2"
+        />
       </div>
     );
   }
