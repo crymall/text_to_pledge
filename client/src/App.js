0df01cd4d55a4764477c98e65a4bc731e5334c4d
Reed Gaines <reed.gaines@gmail.com>
2018-10-21 22:04:55 -0400

    deploy test

--- client/src/App.js
+++ client/src/App.js
@@ -39,12 +39,34 @@ class App extends Component {
           <Line
             className="the-bar"
             percent={((total / 1000) * 100).toString()}
-            strokeWidth="2"
+            strokeWidth="0.5"
             strokeColor="#1E1E1E"
             strokeLinecap="butt"
             trailColor="#E3E3E3"
-            trailWidth="1"
+            trailWidth="0.2"
           />
+          <div className="bar-tics">
+            <div className="tic">
+              <h3> | </h3>
+              <h3> 0 </h3>
+            </div>
+            <div className="tic">
+              <h3> | </h3>
+              <h3> 250 </h3>
+            </div>
+            <div className="tic">
+              <h3> | </h3>
+              <h3> 500 </h3>
+            </div>
+            <div className="tic">
+              <h3> | </h3>
+              <h3> 750 </h3>
+            </div>
+            <div className="tic">
+              <h3> | </h3>
+              <h3> 1K </h3>
+            </div>
+          </div>
         </div>
       </div>
     );
