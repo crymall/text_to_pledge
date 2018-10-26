1659fee9e7910ae543f05e89833efd0ad388159a
Reed Gaines <reed.gaines@gmail.com>
2018-10-26 16:11:40 -0400

    fix bug

--- db/queries.js
+++ db/queries.js
@@ -7,7 +7,7 @@ const badWords = require("./badwords");
 // ROUTING FUNCTIONS
 
 // handleResponse contains all of the routing logic for responding to donors' messages.
-const handleResponse = (req, res, next) => {
+const handleResponse = async (req, res, next) => {
   let message = req.body.Body;
   let splitMsg = message.split(" ").filter(el => {
     return el;
@@ -15,49 +15,40 @@ const handleResponse = (req, res, next) => {
   let noBadWords = splitMsg.every(el => {
     return !badWords[el.toLowerCase()] && el.length < 30;
   });
+
+  let donorExists = await db_actions.donorExists(req.body.From);
+
   // check if donor exists
-  if (noBadWords && message.length < 140) {
-    db_actions
-      .donorExists(req.body.From)
-      .then(donor => {
-        if (donor) {
-          // insert text into db
-          db_actions.addMsg(req.body, donor).then(() => {
-            // take them through process of pledging
-            switch (donor.steps) {
-              case 0:
-                steps.stepOne(req.body);
-                break;
-              case 1:
-                steps.stepTwo(req.body);
-                break;
-              case 2:
-                steps.stepThree(req.body);
-                break;
-              default:
-                console.log(err);
-                break;
-            }
-            res.status(200).send({ status: "OK" });
-          });
-        } else {
-          // if they don't already exist in db, create them
-          db_actions.createDonor(req.body).then(() => {
-            db_actions.addPledge(req.body).then(() => {
-              msg_actions.sendMsg(
-                req.body.From,
-                "Thank you for your generous pledge in support of Pursuit's mission. What's your full name? (In this format: Firstname Lastname)"
-              );
-            });
-          });
-          res.status(200).send({ status: "OK" });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  if (noBadWords && message.length < 140 && donorExists.length) {
+    // insert text into db
+    db_actions.addMsg(req.body, donor).then(() => {
+      // take them through process of pledging
+      switch (donor.steps) {
+        case 0:
+          steps.stepOne(req.body);
+          break;
+        case 1:
+          steps.stepTwo(req.body);
+          break;
+        case 2:
+          steps.stepThree(req.body);
+          break;
+        default:
+          console.log(err);
+          break;
+      }
+      res.status(200).send({ status: "OK" });
+    });
   } else {
-    msg_actions.sendMsg(req.body.From, "Sorry, please try again.");
+    // if they don't already exist in db, create them
+    db_actions.createDonor(req.body).then(() => {
+      db_actions.addPledge(req.body).then(() => {
+        msg_actions.sendMsg(
+          req.body.From,
+          "Thank you for your generous pledge in support of Pursuit's mission. What's your full name? (In this format: Firstname Lastname)"
+        );
+      });
+    });
     res.status(200).send({ status: "OK" });
   }
 };
